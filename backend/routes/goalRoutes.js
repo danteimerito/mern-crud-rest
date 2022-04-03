d2413b1de11d5c7984998b214157a4975de624a1
danteimerito <danteimerito@gmail.com>
2022-04-03 17:51:35 -0400

    completed authentication and route protection

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,20 +7,22 @@ const {
     deleteGoal 
 } = require('../controllers/goalController')
 
+const {protect} = require('../middleware/authMiddleware')
+
 //get goal
-router.get('/', getGoals)
+router.get('/', protect, getGoals)
 
 //set goal
-router.post('/', setGoal)
+router.post('/', protect, setGoal)
 
 //update goal
-router.put('/:id', updateGoal)
+router.put('/:id', protect, updateGoal)
 
 //delete goal
-router.delete('/:id', deleteGoal)
+router.delete('/:id', protect, deleteGoal)
 
-// //condensed version:
-// router.route('/').get(getGoals).post(setGoal)
-// router.route('/:id').put(updateGoal).delete(deleteGoal)
+// // //condensed version:
+// router.route('/').get(protect, getGoals).post(protect, setGoal)
+// router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
