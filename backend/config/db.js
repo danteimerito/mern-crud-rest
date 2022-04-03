467770a8884b45a045f7f7d84b4406c9c0a3d42a
danteimerito <danteimerito@gmail.com>
2022-04-03 00:43:05 -0400

    REST API for goals

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+
+//connect to db using mongoose
+
+const mongoose = require('mongoose')
+
+// **all mongoose methods are asychronous - return a promise
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
