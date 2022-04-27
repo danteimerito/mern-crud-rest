49339e2131c27580a35fdf0f09caf1e2fa018d62
danteimerito <danteimerito@gmail.com>
2022-04-26 23:39:39 -0400

    add, delete and display goals

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,13 +74,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const getMe = asyncHandler(async (req, res) => {
-    const {_id, name, email} = await User.findById(req.user.id)
-
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-    })
+    res.status(200).json(req.user)
 })
 
 //Generate JWT
